055880d52c1b15feb81e85d89d51efdc8d64f628
Junior Iqfar <junioriqfar@gmail.com>
2024-11-18 21:43:48 +0700

    first commit

---
+++ Form1.vb
@@ -0,0 +1,5 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles Me.Load
+        WebView21.Source = New Uri("https://peken.surabaya.go.id")
+    End Sub
+End Class
